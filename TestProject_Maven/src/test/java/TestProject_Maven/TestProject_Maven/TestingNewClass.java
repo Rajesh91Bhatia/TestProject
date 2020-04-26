e9d9db196f9198337b2eeefda507084cd41b8a6f
Node <Node@DESKTOP-FEJ2M74>
2020-04-26 21:35:29 +0530

    Updated

---
+++ TestProject_Maven/src/test/java/TestProject_Maven/TestProject_Maven/TestingNewClass.java
@@ -0,0 +1,10 @@
+package TestProject_Maven.TestProject_Maven;
+
+import org.testng.annotations.Test;
+
+public class TestingNewClass {
+  @Test
+  public void f() {
+	  System.out.println("Testing New Class");
+  }
+}
