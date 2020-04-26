019d5e7176ccf5bc6f60c393a845e161c26a4090
Node <Node@DESKTOP-FEJ2M74>
2020-04-26 12:23:36 +0530

    Added the Reporting File

---
+++ TestProject_Maven/src/test/java/TestProject_Maven/TestProject_Maven/ReportingTest.java
@@ -0,0 +1,22 @@
+package TestProject_Maven.TestProject_Maven;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+
+public class ReportingTest {
+  @Test
+  public void f() {
+	  System.out.println("Actual Test");
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+	  System.out.println("before Method");
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+	  System.out.println("After Method");
+  }
+
+}
